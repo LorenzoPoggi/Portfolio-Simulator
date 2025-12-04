7d4ea0e08fa21448152713cfbb4507c47f6c14f2
Lorenzo Poggi <lorenzo.poggijanin@gmail.com>
2025-12-04 18:40:55 -0300

    fix: modificaciones en los modelos pydantic

--- Backend/app/schemas/schemas.py
+++ Backend/app/schemas/schemas.py
@@ -47,3 +47,29 @@ class Stock_Response(BaseModel):
 # ----------------------------------------------------
 # Portfolio Schemas
 # ----------------------------------------------------
+
+# Esquema de creacion de inversiones en el portfolio
+class Portfolio_Create(BaseModel):
+    stock_id: int
+    quantity: int
+
+# Esquema para devolucion del portfolio
+class Portfolio_View(BaseModel):
+    id: int 
+    user_id: int
+    stock_id: int
+    quantity: int
+    buy_price: float
+    total_invested: float
+    buy_date: datetime
+    average_price: float
+    stock: Optional[Stock_Response]
+    # Clase para convertir de objeto sqlalchemy a este modelo, FastAPI lo interpreta 
+    class Config:
+        from_attributes = True 
+
+# Esquema para actualizar el portfolio
+class Portfolio_Update(BaseModel):
+    quantity: Optional[int]
+    buy_price: Optional[float]
+    average_price: Optional[float]
