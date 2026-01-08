0655ca74677f2571334fd86127fb6a745c58e1b0
Lorenzo Poggi <lorenzo.poggijanin@gmail.com>
2026-01-07 23:44:54 -0300

    chore: agrego la estructura inicial para el Frontend

--- Backend/app/routers/authentication.py
+++ Backend/app/routers/authentication.py
@@ -1,6 +1,9 @@
 # authentication.py
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status, Request, Form
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from database.database import get_db
 from core.exceptions import excepciones
@@ -10,8 +13,10 @@ from database.models import *
 from schemas.user import *
 from schemas.token import Login_Response
 
-# Inicializacion del Router
+# Inicializacion del Router y Templates
 router = APIRouter(tags=['Authentication'], prefix='/authentication')
+router.mount("/static", StaticFiles(directory="Frontend/static", html=True), name="static")
+templates = Jinja2Templates(directory='Frontend/templates')
 
 # ----------------------------------------------------
 # Operaciones CRUD para Register y Login
@@ -48,3 +53,19 @@ async def login_user(user: User_Login, db: Session = Depends(get_db)):
         'token_type': 'bearer',
         'user': db_user
     }
+
+# ----------------------------------------------------
+# Operaciones INTERFAZ para Register y Login
+# ----------------------------------------------------
+
+# Operacion para renderizar el registro a una interfaz
+@router.get('/register', response_class= HTMLResponse)
+async def register_html(request: Request):
+    return templates.TemplateResponse(
+        request= request, name= 'authentication.html')
+
+# Operacion para renderizar el inicio de sesion a una interfaz
+@router.get('/login', response_class= HTMLResponse)
+async def login_html(request: Request):
+    return templates.TemplateResponse(
+        request= request, name= 'authentication.html')
---
+++ Frontend/static/js/script.js
@@ -0,0 +1,1 @@
+// archivo para la interactividad del proyecto
